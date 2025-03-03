32419814a9b5619b71497a0c9c420170f25a44f0
PrinceVegeta447 <vegetakun447@gmail.com>
2025-03-03 18:46:09 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "1710597756"
-    sudo_users = [1710597756]
-    GROUP_ID = -1002483506913
-    TOKEN = "7568918921:AAHT6_qx76-7hyG74S2T0eG1R1yWgqzXmWo"
+    OWNER_ID = "6523029979"
+    sudo_users = [6523029979]
+    GROUP_ID = -1002455955347
+    TOKEN = "7775101187:AAEctjmSG-xhVYR8jmQjT_RX4G6R3dPbkLA"
     mongo_url = "mongodb+srv://vegetakun447:TK1WRYfAESFT5vTe@cluster0.hcngy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["http://ibb.co/WvZ6gLTr"]
-    SUPPORT_CHAT = "CollectYourLegends"
-    UPDATE_CHAT = "CollectYourLegends"
-    BOT_USERNAME = "CollecDBLegendsBot"
-    CHARA_CHANNEL_ID = "-1002236620616"
+    SUPPORT_CHAT = "TBD"
+    UPDATE_CHAT = "TBD"
+    BOT_USERNAME = "test532267bot"
+    CHARA_CHANNEL_ID = "-1002455955347"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
     
